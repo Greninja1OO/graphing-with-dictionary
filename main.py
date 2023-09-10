ae3faa5e40e0700d151b035c9da3c2fbd2ce573c
Sanjeev <usanjeev2004@gmail.com>
2023-09-10 21:00:59 +0530

    polygon ver(indicating closed loops)

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ root.geometry("1000x750")
 root.title("Graphing cuz i cant come up with better name")
 canvas = Canvas(root, width=1000, height=1000)
 canvas.pack()
-dict={1: [2, 3, 4, 5], 2:[1, 3, 4, 5]}
+dict={1: [1,2, 3, 5], 2:[4,1,6,7],4:[1]}
 l1=[]
+def closed_loop(i,j):
+    if(i==j):
+        return 1
+    if(j in dict):
+        for k in dict[j]:
+            if(k!=j and closed_loop(i,k)):
+                return 1
 for i in dict:
     if(i not in l1):
         l1.append(i)
@@ -30,8 +37,10 @@ for i in l1:
                 c2 = 300 + 220 * (math.sin(l1.index(i) * 2 * math.pi / len(l1)))
                 c3  = 300 + 210 * (math.cos(l1.index(i) * 2 * math.pi / len(l1)))
                 c4 = 300 + 210 * (math.sin(l1.index(i) * 2 * math.pi / len(l1)))
-                canvas.create_oval(c1 - 10, c2 - 10, c1 + 10, c2 + 10, outline='black',)
-                canvas.create_line(c3,c4,c3,c4,arrow="last")
+                canvas.create_oval(c1 - 10, c2 - 10, c1 + 10, c2 + 10, outline='red',)
+                canvas.create_line(c3,c4,300 + 210 * (math.cos(l1.index(i) * 2 * math.pi / len(l1)+0.03)),300 + 210 * (math.sin(l1.index(i) * 2 * math.pi / len(l1)+0.03)),arrow="last",fill="red")
+            elif(closed_loop(i,j)):
+                canvas.create_line(c1,c2,c3,c4,arrow="last",fill="red" )
             else:
-                canvas.create_line(c1,c2,c3,c4,arrow="last" )
+                canvas.create_line(c1, c2, c3, c4, arrow="last" )
 root.mainloop()
